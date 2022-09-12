02f72c80b8aa933d0e19ea57a713ebf1df0c012f
ahmadshakir21 <ahmad.software.developer@gmail.com>
2022-09-12 11:54:44 +0300

    Another Commit

---
+++ lib/constant/image.dart
@@ -0,0 +1,10 @@
+class Image {
+  Image._();
+
+  static const String cloudy = "assets/animation/113873-clouds.json";
+  static const String rain = "assets/animation/118442-rain.json";
+  static const String sunny = "assets/animation/61114-clear-day.json";
+  static const String snow =
+      "assets/animation/31778-snowflake-loading-screen.json";
+  static const String foggy = "assets/animation/4791-foggy.json";
+}
